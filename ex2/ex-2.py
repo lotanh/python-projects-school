a4390b95cf32538dc0ad71bb95f979f7f5bcf85a
Lotan Hadad <lotan3600@gmail.com>
2022-04-03 21:43:38 +0300

    Added lines

--- ex2/ex-2.py
+++ ex2/ex-2.py
@@ -2,10 +2,12 @@ import numpy as np
 
 suger_range = int(np.floor(np.random.random() * 10 + 1))
 
-arr = np.ones((16,21))
+arr = np.random.randint(0,suger_range, size=(16,21))
 
-for i in range(0,suger_range):` `
-    arr[i] = i
-print(arr)
+def get_suger_avg(array):
+    avg = np.average(array, axis=1)
+    return avg
+
+print(get_suger_avg(arr))
 
 
